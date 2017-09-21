0ff70a285fa22f95ada438ea65019c182a98428e
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-21 13:04:15 +1000

    crab hands

--- 04-ruby/crappy_calc/app.rb
+++ 04-ruby/crappy_calc/app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+# require 'pry'
 
 get '/' do
   erb :index
 end
+
+get '/answer' do
+  @num1 = params[:first].to_i
+  @num2 = params[:second].to_i
+  erb :answer
+end
+
+# binding.pry
